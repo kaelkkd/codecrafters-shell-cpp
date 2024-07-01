99b6102ebd65762009e18c6f3b8fc323760a0a68
kael <kaelsiebra233@gmail.com>
2024-07-01 16:02:30 -0300

    passed stage 6

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
 
+enum commands {
+  quit,
+  echo,
+  invalid,
+  type
+};
+
+commands checkCommand(std::string command) {
+  if (command == "exit" || command == "exit 0") return quit;
+  else if (command.substr(0, 4) == "echo") return echo;
+  else if (command.substr(0, 4) == "type") return type;
+
+  else return invalid;
+}
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
@@ -11,18 +26,23 @@ int main() {
     std::string input;
     std::getline(std::cin, input);
     
-    if (input == "exit 0") {
-      return 0;
+    switch (checkCommand(input)) {
+      case invalid:
+        std::cout << input << ": command not found" << std::endl;
+        break;
+      case quit:
+        return 0;
+      case echo:
+        std::cout << input.substr(5, input.length()) << std::endl;
+        break;
+      case type:
+        std::string verType = input.substr(5, input.length());
+        if (checkCommand(verType) != invalid) {
+          std::cout << verType << " is a shell builtin" << std::endl;
+        }
+        else {
+          std::cout << verType << ": not found" << std::endl;
+        }
     }
-
-    else if (input.substr(0, 5) == "echo ") {
-      std::cout << input.substr(5, input.length()) << std::endl;
-    }
-
-    else {
-      std::cout << input << ": command not found" << std::endl;
-    }
-    
-  }
-  
+  } 
 }
